c1e07c2dcfe8144c5bb9b6d37b0c8c9ff68339ed
MadnexTheSer <vvomifares@gmail.com>
2022-07-27 10:16:56 +0100

    Completed part 18/52 in the journey of creating Java Chess Engine.

--- src/com/chess/engine/player/BlackPlayer.java
+++ src/com/chess/engine/player/BlackPlayer.java
@@ -2,6 +2,7 @@ package com.chess.engine.player;
 
 import java.util.Collection;
 
+import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 import com.chess.engine.pieces.Piece;
@@ -19,5 +20,13 @@ public class BlackPlayer extends Player{
 	public Collection<Piece> getActivePieces() {
 		return this.board.getBlackPieces();
 	}
-
+	@Override
+	public Alliance getAlliance() {
+		return Alliance.BLACK;
+	}
+	@Override
+	public Player getOpponent() {
+		return this.board.whitePlayer();
+	}
+	
 }
--- src/com/chess/engine/player/WhitePlayer.java
+++ src/com/chess/engine/player/WhitePlayer.java
@@ -2,6 +2,7 @@ package com.chess.engine.player;
 
 import java.util.Collection;
 
+import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 import com.chess.engine.pieces.Piece;
@@ -18,6 +19,15 @@ public class WhitePlayer extends Player{
 	@Override
 	public Collection<Piece> getActivePieces() {
 		return this.board.getBlackPieces();
+	}	
+	@Override
+	public Alliance getAlliance() {
+		return Alliance.WHITE;
+	}
+
+	@Override
+	public Player getOpponent() {
+		return this.board.blackPlayer();
 	}
 
 
